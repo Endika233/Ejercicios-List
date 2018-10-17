757218b3d9bc97c4160e4aa8b2aaeb02ae8456e1
Endika233 <43742370+Endika233@users.noreply.github.com>
2018-10-17 14:22:18 +0200

    Finalizado Decimales

--- Ejercicios List/Program.cs
+++ Ejercicios List/Program.cs
@@ -26,9 +26,17 @@ namespace Ejercicios_List
             //Lee por consola los números que introduzca el usuario.
             //Si el número introducido es decimal lo guardará en una lista de números decimales,.
             //Si no es decimal entonces los guardará en una lista de ints.
+            //Una vez haya introducido un número negativo:
+            //Muestra cuántos números decimales y cuantos enteros ha creado.
+            //Haz la suma de todos los números enteros introducidos.
+            //Saca la media de todos los números, tanto enteros como decimales.
+            //Controla las posibles excepciones que se puedan dar, como por ejemplo introducir un número negativo.
+
             List<int> listaEnteros = new List<int>();
             List<double> listaDecimales = new List<double>();
             string respuesta;
+            double sumaDecimales = 0,mediaTotal;
+            int sumaEnteros=0;
             do
             {
                 Console.WriteLine("Escriba un número");
@@ -45,12 +53,25 @@ namespace Ejercicios_List
                 {
                     try
                     {
-                        listaDecimales.Add(Convert.ToInt32(respuesta));
+                        listaEnteros.Add(Convert.ToInt32(respuesta));
                     }
                     catch { Console.WriteLine("El tipo de dato introducido no es correcto"); }
                 }
 
             } while (!respuesta.Contains("-"));
+            Console.WriteLine("La lista de números decimales contiene " + listaDecimales.Count() + " números y la lista de números enteros " + listaEnteros.Count());
+            foreach (int x in listaEnteros)
+            {
+                sumaEnteros=sumaEnteros+x;
+            }
+            Console.WriteLine("La suma de todos los enteros es " + sumaEnteros);
+            foreach (double x in listaDecimales)
+            {
+                sumaDecimales = sumaDecimales + x;
+            }
+            mediaTotal = (Convert.ToDouble(sumaEnteros) + sumaDecimales) / Convert.ToDouble(listaEnteros.Count() + listaDecimales.Count());
+            Console.WriteLine("La media global es "+mediaTotal);
+            Console.ReadKey();
         }
 
         //public static void CrearLista(List<string> listaTipo)
