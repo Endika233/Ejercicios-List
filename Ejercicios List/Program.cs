a000503c681f1fb616c7c231f86a047cd0da7420
Endika233 <43742370+Endika233@users.noreply.github.com>
2018-10-17 13:33:43 +0200

    Ejercicio decimales finalizado

--- Ejercicios List/Program.cs
+++ Ejercicios List/Program.cs
@@ -10,52 +10,84 @@ namespace Ejercicios_List
     {
         static void Main(string[] args)
         {
-            List<string> listaTipo = new List<string>();
-            CrearLista(listaTipo);
-            Console.WriteLine("La lista tiene " + ContarElementos(listaTipo) + " elementos");
-            MostrarLista(listaTipo);
-            OrdenarLista(listaTipo);
-            MostrarLista(listaTipo);
-            BorrarDeLaLista(listaTipo, "Pan");
-            MostrarLista(listaTipo);
-            Console.WriteLine("La lista tiene " + ContarElementos(listaTipo) + " elementos");
-            Console.ReadLine();
-        }
-        public static void CrearLista(List<string> listaTipo)
-        {
-            listaTipo.Add("Pan");
-            listaTipo.Add("Hamburguesas");
-            listaTipo.Add("Cerveza");
-        }
-        public static void MostrarLista(List<string> listaTipo)
-        {
-            for (int i = 0; i < listaTipo.Count; i++)
+            //Primer Ejercicio
+            //List<string> listaTipo = new List<string>();
+            //CrearLista(listaTipo);
+            //Console.WriteLine("La lista tiene " + ContarElementos(listaTipo) + " elementos");
+            //MostrarLista(listaTipo);
+            //OrdenarLista(listaTipo);
+            //MostrarLista(listaTipo);
+            //BorrarDeLaLista(listaTipo, "Pan");
+            //MostrarLista(listaTipo);
+            //Console.WriteLine("La lista tiene " + ContarElementos(listaTipo) + " elementos");
+            //Console.ReadLine();
+
+            //Segundo Ejercicio
+            //Lee por consola los números que introduzca el usuario.
+            //Si el número introducido es decimal lo guardará en una lista de números decimales,.
+            //Si no es decimal entonces los guardará en una lista de ints.
+            List<int> listaEnteros = new List<int>();
+            List<double> listaDecimales = new List<double>();
+            string respuesta;
+            do
             {
-                Console.WriteLine(listaTipo.ElementAt(i));               
-            }
+                Console.WriteLine("Escriba un número");
+                respuesta = Console.ReadLine();
+                if (respuesta.Contains(",") || respuesta.Contains("."))
+                {
+                    try
+                    {
+                        listaDecimales.Add(Convert.ToDouble(respuesta));
+                    }
+                    catch { Console.WriteLine("El tipo de dato introducido no es correcto"); }
+                }
+                else
+                {
+                    try
+                    {
+                        listaDecimales.Add(Convert.ToInt32(respuesta));
+                    }
+                    catch { Console.WriteLine("El tipo de dato introducido no es correcto"); }
+                }
+
+            } while (!respuesta.Contains("-"));
         }
-        public static void OrdenarLista(List<string> listaTipo)
-        {
-            listaTipo.Sort();////Este método ordena directamente
-        }
-        public static void BorrarDeLaLista(List<string> listaTipo,string datoBorrar)
-        {
-            listaTipo.Remove(datoBorrar);
-        }
-        public static int ContarElementos(List<string> listaTipo)
-        {
-            return listaTipo.Count();
-        }
+
+        //public static void CrearLista(List<string> listaTipo)
+        //{
+        //    listaTipo.Add("Pan");
+        //    listaTipo.Add("Hamburguesas");
+        //    listaTipo.Add("Cerveza");
+        //}
+        //public static void MostrarLista(List<string> listaTipo)
+        //{
+        //    for (int i = 0; i < listaTipo.Count; i++)
+        //    {
+        //        Console.WriteLine(listaTipo.ElementAt(i));               
+        //    }
+        //}
+        //public static void OrdenarLista(List<string> listaTipo)
+        //{
+        //    listaTipo.Sort();////Este método ordena directamente
+        //}
+        //public static void BorrarDeLaLista(List<string> listaTipo,string datoBorrar)
+        //{
+        //    listaTipo.Remove(datoBorrar);
+        //}
+        //public static int ContarElementos(List<string> listaTipo)
+        //{
+        //    return listaTipo.Count();
+        //}
+        //Hay que crear 5 funciones:
+        //Método CreaLista:
+        //Esta función recibirá por parámetro la lista que crearemos en el programa y la devolverá escrita.
+        //Método MostrarLista:
+        //Esta función simplemente recibirá la lista por parámetro y la mostrará en pantalla.
+        //Crea un método OrdenarLista:
+        //Ordena alfabéticamente la lista que reciba por parámetro.
+        //Crea un método BorrarDeLaLista:
+        //Recibirá un elemento y una lista por parámetro. Si encuentra el elemento en la lista lo borrara.
+        //Crea un método ContarElementos:
+        //Recibe una lista por parámetro y devuelve la cantidad de elementos en ella.
     }
 }
-//Hay que crear 5 funciones:
-//Método CreaLista:
-//Esta función recibirá por parámetro la lista que crearemos en el programa y la devolverá escrita.
-//Método MostrarLista:
-//Esta función simplemente recibirá la lista por parámetro y la mostrará en pantalla.
-//Crea un método OrdenarLista:
-//Ordena alfabéticamente la lista que reciba por parámetro.
-//Crea un método BorrarDeLaLista:
-//Recibirá un elemento y una lista por parámetro. Si encuentra el elemento en la lista lo borrara.
-//Crea un método ContarElementos:
-//Recibe una lista por parámetro y devuelve la cantidad de elementos en ella.
